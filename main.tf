e5aaa5ffcfd9fcfec04e55cc27d439aed02e33b0
carlosarias1501 <carlosarias1501@gmail.com>
2025-07-15 22:04:27 -0300

    Change

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
+######## Provider #########
 provider "aws" {
   region = "us-east-1"
+}
+
+######## Resource #########
+resource "aws_instance" "ngnix_server" {
+  ami           = "ami-0440d3b780d96b29d"
+  instance_type = "t3.micro"
 }
